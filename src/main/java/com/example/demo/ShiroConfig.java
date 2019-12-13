f21d336af3bc74c014a82afcacc6b9691c1ee56b
hmr <2335883446@qq.com>
2019-12-13 14:28:03 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/ShiroConfig.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Configuration
+public class ShiroConfig {
+    @Bean
+    MyRealm myRealm() {
+        return new MyRealm();
+    }
+
+    @Bean
+    ShiroFilterFactoryBean shiroFilterFactoryBean() {
+        DefaultWebSecurityManager manager = new DefaultWebSecurityManager();
+        manager.setRealm(myRealm());
+        ShiroFilterFactoryBean bean = new ShiroFilterFactoryBean();
+        bean.setSecurityManager(manager);
+        bean.setLoginUrl("/login");
+        bean.setSuccessUrl("/index");
+        bean.setUnauthorizedUrl("/unauthorizedurl");
+        Map<String, String> map = new LinkedHashMap<>();
+        map.put("/doLogin", "anon");
+        map.put("/**", "authc");
+        bean.setFilterChainDefinitionMap(map);
+        return bean;
+    }
+}
